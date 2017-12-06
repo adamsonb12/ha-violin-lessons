8ba19891dd91f1c310172789f619ef00e061f305
Adamson <AdamsonBH@ad.mtc.byu.edu>
2017-12-06 11:31:14 -0700

    connected to new mongo db, connected homeroutes to index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+require('./routes/homeRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
